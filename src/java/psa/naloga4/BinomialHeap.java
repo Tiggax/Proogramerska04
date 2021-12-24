128ecb99aa31a8c3d34ba8c0cb3a149cc02d9c52
Tiggax <gimpelj.tigi@gmail.com>
2021-12-24 20:57:23 +0100

    prva initializacija inserta, hope it works

--- src/java/psa/naloga4/BinomialHeap.java
+++ src/java/psa/naloga4/BinomialHeap.java
@@ -10,15 +10,29 @@ public class BinomialHeap {
 	}
 
 	public boolean insert(int key) {
-// True <- vstavljen
-// False <- ni bil/ je ze not
-// IF array mejhen ga resizaš.
-//----------------------------
+		// True <- vstavljen
+		// False <- ni bil/ je ze not
+		// IF array mejhen ga resizaš.
+		// ----------------------------
 
-		if (condition) {
-			
-		}
+		BinomialNode tmp = new BinomialNode(key);
 		
+		for (int i = 0; i < data.length-1; i++) {
+			if (!(data[i]==null)) {
+				if ((data[i+1]==null)) {
+					if (i+1>data.length) {
+						resizeArray();
+						data[i+1] = merge(data[i], tmp);
+						return true;
+					}
+					data[i+1] = merge(data[i], tmp);
+					return true;
+				}
+			}
+			data[i] = tmp;
+			return true;
+		}
+		return false;
 	}
 
 	public int getMin() {
@@ -30,22 +44,21 @@ public class BinomialHeap {
 	}
 
 	private void resizeArray() {
-		BinomialNode[] tmp = new BinomialNode[data.length*2];
+		BinomialNode[] tmp = new BinomialNode[data.length * 2];
 		for (int i = 0; i < tmp.length; i++) {
-			tmp[i]=data[i];
+			tmp[i] = data[i];
 		}
 		this.data = tmp;
 	}
 
 	private BinomialNode merge(BinomialNode t1, BinomialNode t2) {
-// vzam manši root, pinaš večjega nanga,
+		// vzam manši root, pinaš večjega nanga,
 
+		if (t1.getKey()==t2.getKey()) {System.out.println("hmmm...");}
+	
 		if ( t1.getKey() < t2.getKey() ) {
 			t1.addChild(t2);
 		} else if ( t1.getKey() > t2.getKey() ) {
 			t2.addChild(t1);
-		} else {
-			
-		}
 	}
 }

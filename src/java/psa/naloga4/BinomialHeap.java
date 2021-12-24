e8f438b0ab7b82400caa572dee79cf49ba7b21a8
Tiggax <gimpelj.tigi@gmail.com>
2021-12-24 20:27:04 +0100

    made resizeArray, started merge()

--- src/java/psa/naloga4/BinomialHeap.java
+++ src/java/psa/naloga4/BinomialHeap.java
@@ -4,29 +4,48 @@ import java.util.Vector;
 
 public class BinomialHeap {
 	BinomialNode[] data;
-	
-	BinomialHeap(){
+
+	BinomialHeap() {
 		data = new BinomialNode[1];
 	}
-	
+
 	public boolean insert(int key) {
-		throw new UnsupportedOperationException("To funkcijo morate implementirati");
+// True <- vstavljen
+// False <- ni bil/ je ze not
+// IF array mejhen ga resizaš.
+//----------------------------
+
+		if (condition) {
+			
+		}
+		
 	}
-	
+
 	public int getMin() {
-		throw new UnsupportedOperationException("To funkcijo morate implementirati");
+
 	}
-	
+
 	public boolean delMin() {
-		throw new UnsupportedOperationException("To funkcijo morate implementirati");
+
 	}
-	
+
 	private void resizeArray() {
-		throw new UnsupportedOperationException("To funkcijo morate implementirati");
+		BinomialNode[] tmp = new BinomialNode[data.length+1];
+		for (int i = 0; i < tmp.length; i++) {
+			tmp[i]=data[i];
+		}
+		this.data = tmp;
 	}
-	
+
 	private BinomialNode merge(BinomialNode t1, BinomialNode t2) {
-		throw new UnsupportedOperationException("To funkcijo morate implementirati");
+// vzam manši root, pinaš večjega nanga,
+
+		if ( t1.getKey() < t2.getKey() ) {
+			t1.addChild(t2);
+		} else if ( t1.getKey() > t2.getKey() ) {
+			t2.addChild(t1);
+		} else {
+			
+		}
 	}
 }
-
